8d0e3afd876ed1410cefdd666bf6ca5f91989525
rmcs87 <rmcs87@gmail.com>
2020-04-30 14:07:31 -0300

    Identifying Entities.

--- TEP.Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace TEP.Domain
-{
-    public class Class1
-    {
-    }
-}
---
+++ TEP.Domain/Entities/EntityBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TEP.Domain.Entities
+{
+    class EntityBase
+    {
+        public int Id { get; set; }
+    }
+}
