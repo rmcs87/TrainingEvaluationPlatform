6b89c9aa4d5f84a032e309ab6b6f328696ccaa8e
rmcs87 <rmcs87@gmail.com>
2020-05-08 15:42:46 -0300

    Created Execution Step to be used to build the Execution graph of a Trainning Session. + Start of Tests (study phase)

---
+++ TEP.Domain.Tests/InteractionTests.cs
@@ -0,0 +1,16 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xunit.Sdk;
+using TEP.Domain.Entities;
+
+namespace TEP.Domain.Tests
+{
+    [TestClass]
+    public class InteractionTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //ref: https://docs.microsoft.com/pt-br/visualstudio/test/walkthrough-creating-and-running-unit-tests-for-managed-code?view=vs-2019
+        }
+    }
+}
---
+++ TEP.Domain/Entities/ExecutionStep.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TEP.Domain.ValueObjects;
+using TEP.Shared.ValueObjects;
+
+namespace TEP.Domain.Entities
+{
+    class ExecutionStep : Step
+    {
+        public ExecutionStep(Standard standard, string name, IEnumerable<Action> actions) : base(standard, name)
+        {
+            Actions = actions;
+        }
+
+        public IEnumerable<Action> Actions { get; private set; }
+        public Duration ExecutionTime { get; private set; }
+    }
+}
+}
