c0c832f1bdc83389b47daee9e97aa5550f70f171
rmcs87 <rmcs87@gmail.com>
2020-05-12 14:19:32 -0300

    A LeafStep has only One Interaction.

--- TEP.Domain.Tests/Entities/LeafStepTests.cs
+++ TEP.Domain.Tests/Entities/LeafStepTests.cs
@@ -1,15 +1,37 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace TEP.Domain.Tests.Entities
 {
-    class LeafStepTests
+    [TestClass]
+    public class LeafStepTests
     {
-        /*
-         *Se a duração total esta funcionando
-         *Se o tempo limite esta funcionando
-         * 
-         */
+        public void ProcessTime_for_Interaction()
+        {
+
+        }
+        [TestMethod]
+        public void ExpectedTime_from_Simple_Interaction()
+        {
+
+        }
+
+        public void ExpectedTime_from_Bubble_Interaction()
+        {
+
+        }
+        public void TimeLimit_from_Simple_Interaction()
+        {
+
+        }
+
+        public void TimeLimit_from_Bubble_Interaction()
+        {
+
+        }
+
     }
+
 }
--- TEP.Domain/Entities/LeafStep.cs
+++ TEP.Domain/Entities/LeafStep.cs
@@ -17,24 +17,22 @@ namespace TEP.Domain.Entities
         /// <param name="standard">The classification for a step, which implies how it will be hadled by the trainning APP.</param>
         /// <param name="name">The name fo this Step.</param>
         /// <param name="interactions">The Interactions to be Performed in this Step.</param>
-        public LeafStep(Standard standard, string name, IEnumerable<Interaction> interactions) : base(standard, name)
+        public LeafStep(Standard standard, string name, Interaction interaction) : base(standard, name)
         {
-            Interactions = interactions;
+            Interaction = interaction;
         }
         /// <summary>
         /// Gets all interactions to be performed in this Step.
         /// </summary>
-        public IEnumerable<Interaction> Interactions { get; private set; }
+        public Interaction Interaction { get; private set; }
         /// <summary>
         /// Calculates all times for this step, consifering all Interaction to be performed druing the step.
         /// </summary>
         public override void ProcessDuration()
-        {
-            foreach (var interaction in Interactions)
-            {
-                ExpectedDuration.Increment(interaction.EstimatedTime.Milis);
-                LimitDuration.Increment(interaction.TimeLimit);
-            }
+        {            
+            ExpectedDuration.Increment(Interaction.EstimatedTime.Milis);
+            LimitDuration.Increment(Interaction.TimeLimit);
+
             Processed = true;
         }
     }
