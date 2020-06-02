531ec7fa31707c71240cf0c704b1de821b2b71fb
rmcs87 <rmcs87@gmail.com>
2020-06-02 09:47:04 -0300

    Structuring DB.

---
+++ TEP.Infra.Data/Context/Context.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TEP.Infra.Data.Context
+{
+    public class Context : DbContext
+    {
+
+    }
+}
