ca949194452eb6532daaecdf661af4873b7a9e63
rmcs87 <rmcs87@gmail.com>
2020-06-28 15:23:59 -0300

    Cleaning Projects.

--- TEP.Appication/Interfaces/IAssetApp.cs
+++
@@ -1,9 +0,0 @@
-﻿using TEP.Appication.DTO;
-using TEP.Domain.Entities;
-
-namespace TEP.Appication.Interfaces
-{
-    public interface IAssetApp : IAppBase<Asset, AssetDTO>
-    {
-    }
-}
--- TEP.Appication/Interfaces/ISupervisorApp.cs
+++
@@ -1,9 +0,0 @@
-﻿using TEP.Appication.DTO;
-using TEP.Domain.Entities;
-
-namespace TEP.Appication.Interfaces
-{
-    public interface ISupervisorApp : IAppBase<Supervisor, SupervisorDTO>
-    {
-    }
-}
