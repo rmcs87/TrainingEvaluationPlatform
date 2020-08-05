3afe1c0e16026d39fc0c7994a638c06bb1eb97bd
Ricardo Mendes Costa <ricardo.costa@uvv.br>
2020-08-05 18:24:35 +0000

    Migrating to Codespaces.

--- TEP.Infra.Persistence/DependencyInjection.cs
+++ TEP.Infra.Persistence/DependencyInjection.cs
@@ -1,8 +1,6 @@
-﻿using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using System;
 using TEP.Application.Common.Interfaces;
 
